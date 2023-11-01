619e780c87ee7811cfdd980ad69941e94a92da78
tigger <tigger@cherground.com>
2023-11-01 17:16:19 +0900

    Fetching Timeline

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ const Wrapper = styled.div`
     display: grid;
     gap: 50px;
     grid-template-columns: 1fr 4fr;
-    height: 100px;
+    height: 100%;
     padding: 50px 0;
     width: 100%;
     max-width: 860px;
