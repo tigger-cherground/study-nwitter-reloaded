94817a5c3e7dbf3c7860cc18b14d3b94cf9c9c7c
tigger <tigger@cherground.com>
2023-11-01 12:16:21 +0900

    Log In

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,119 +1,105 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import { styled } from "styled-components";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
-const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-    font-size: 42px;;
-`;
-
-const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-`;
-
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="sumbit"] {
-        cursor: pointer;
-        &:hover {
-            opacity: 0.8;
-        }
-    }
-`;
-
-const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
-`;
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components";
 
 export default function CreateAccount() {
-    const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { target: {name, value}} = e;
-        if (name === "name") {
-            setName(value);
-        } else if (name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
-            setPassword(value);
-        }
-    }
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (isLoading || name === "" || email === "" || password === "") return;
-        try {
-            setIsLoading(true);
-            // create an account
-            const credentials = await createUserWithEmailAndPassword(auth, email, password);
-            console.log(credentials.user);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-            // set the name of the user.
-            await updateProfile(credentials.user, {
-                displayName: name
-            });
-
-            // redirect to the home page
-            navigate("/");
-        } catch(e) {
-            // setError
-        } finally {
-            setIsLoading(false);
-        }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "name") {
+      setName(value);
+    } else if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
     }
-    return (
-      <Wrapper>
-        <Title>Join ✖︎</Title>
-        <Form onSubmit={onSubmit}>
-          <Input
-            name="name"
-            value={name}
-            placeholder="Name"
-            type="text"
-            onChange={onChange}
-            required
-          />
-          <Input
-            name="email"
-            value={email}
-            placeholder="Email"
-            type="email"
-            onChange={onChange}
-            required
-          />
-          <Input
-            name="password"
-            value={password}
-            placeholder="Password"
-            type="password"
-            onChange={onChange}
-            required
-          />
-          <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
-        </Form>
-        { error !== "" ? <Error>{error}</Error> : null }
-      </Wrapper>
-    );
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (isLoading || name === "" || email === "" || password === "") return;
+    try {
+      setIsLoading(true);
+      // create an account
+      const credentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(credentials.user);
+
+      // set the name of the user.
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+
+      // redirect to the home page
+      navigate("/");
+    } catch (e) {
+      // setError
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Join ✖︎</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="name"
+          value={name}
+          placeholder="Name"
+          type="text"
+          onChange={onChange}
+          required
+        />
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          onChange={onChange}
+          required
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          onChange={onChange}
+          required
+        />
+        <Input
+          type="submit"
+          value={isLoading ? "Loading..." : "Create Account"}
+        />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account? <Link to="/login">Log in &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,79 @@
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "../components/auth-components";
+
 export default function Login() {
-    return <h1>Login</h1>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (isLoading || email === "" || password === "") return;
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (e) {
+      // setError
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Log into ✖︎</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          onChange={onChange}
+          required
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          onChange={onChange}
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?{" "}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
